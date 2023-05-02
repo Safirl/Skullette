ed8da6cd4b8587dc4344b2c81ee9d3533622bce6
loic leforestier <loic.leforestier@mmibordeaux.com>
2023-05-02 17:30:47 +0200

    adding one way platform

---
+++ Skullette/Assets/Scripts/GroundScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundScript : MonoBehaviour
+{
+    public CameraController cameraController;
+
+    public Collider2D myCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
