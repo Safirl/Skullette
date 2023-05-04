b54a6b29e18499cdb902bfa3fe61be2ada74125c
Thai-Son-TQ <thai-son.tran-quang@mmibordeaux.com>
2023-05-04 11:00:01 +0200

    ui menu

---
+++ Skullette/Assets/Scripts/ShowLore.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCredits : MonoBehaviour
+{
+    public GameObject gameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowBtn()
+    {
+        gameObject.SetActive(true);
+    }
+
+
+    public void CloseBtn()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
